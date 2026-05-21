6192b806f7b7c20f3718be9872b0c14371b24c71
lunalobos-ungs <lunalobosdev@gmail.com>
2026-05-21 00:45:29 -0300

    Readme

---
+++ src/juego/Contexto.java
@@ -0,0 +1,33 @@
+package juego;
+
+/**
+ * La idea de esta interfaz es que su implementación condense la
+ * información de todos los elementos renderizados en pantalla
+ * con el fin de poder ser consultada por cada elemento.
+ *
+ * @author Miguel Angel Luna Lobos
+ */
+public interface Contexto {
+
+    /**
+     * Este método detecta el elemento con el cual colisiona el elemento argumento, puede devolver null.
+     *
+     * @param elemento el elemento del cual queremos saber si está en colisión.
+     * @return el elemento con el cual colisiona el argumento o null si no está en colisión.
+     */
+    Elemento enColisionCon(Elemento elemento);
+
+    /**
+     * Agrega un elemento al contexto, el mismo debe renderizarse en
+     * pantalla de manera independiente a las implementaciones de esta interfaz.
+     * @param elemento el elemento a agregar
+     */
+    void agregar(Elemento elemento);
+
+    /**
+     * Elimina un elemento al contexto, el mismo debe dejar de renderizarse
+     * en pantalla de manera independiente a las implementaciones de esta interfaz.
+     * @param elemento el elemento a quitar
+     */
+    void quitar(Elemento elemento);
+}
