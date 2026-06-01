927c58e3ce847a713dcff6ef3546cd5bd8faa2fa
¨Noelia¨ <nbarrionuevob26@gmail.com>
2026-06-01 04:35:57 -0300

    Se implementa control de posiciones de enemigos. pendiente implementacion de colision

--- src/juego/Enemigo.java
+++ src/juego/Enemigo.java
@@ -1,16 +1,57 @@
 package juego;
 
+import java.awt.Image;
+import java.time.Instant;
+import java.util.Objects;
+
+import javax.swing.ImageIcon;
+
 import entorno.Entorno;
 
 /**
- * Representa un enemigo del juego. Implementación provisional;
- * la mayoría de sus métodos aún no están implementados.
+ * Representa los enemigos del juego;
+ * Aparecen los enemigos por derecha e izquiera de forma aleatoria 
+ * avanzan al lado opuesto al que aparecen
+ * pueden superponerce entre si 
+ * no pueden atravesar las islas
+ * cuando llegan al borde de la pantalla se vuelven null
+ * siempre debe tener una cantidad minima de enemigos vivos en pantalla
+ * colision con princesa 
+ * colision con proyectiles
  *
  */
 public class Enemigo implements Elemento {
+	private double x;
+	private double y;
+	private double ancho;
+	private double alto;
+    private final Image enemigoOriginal;
+    private Image enemigo;
+    private final int id;
+    private final double velocidad;
+    private double velocidadCaidaLibre;
+    private double aceleracionGravitatoria;
+    private double angulo;
+    private Instant marcaTemporalDeCaida;
+    
+    Enemigo (GeneradorId generadorId, Entorno entorno, double x , double y, double angulo  ){
+        this.x = x;
+        this.y = y ;
+        ancho = 40.0;
+        alto = 40.0;
+        enemigoOriginal = new ImageIcon(Objects.requireNonNull(this.getClass().getResource("enemigo.png"))).getImage();
+        enemigo = enemigoOriginal.getScaledInstance((int) ancho, (int) alto, Image.SCALE_DEFAULT);
+        id = generadorId.nuevoId();
+        velocidad = 1.0;
+        velocidadCaidaLibre= 4.0;
+        // luego el angulo se modifica entonces tendremos que hacer this.angulo
+        this.angulo = angulo ;
+        aceleracionGravitatoria = 10.0;
+        //cayendo();
+        }
     @Override
     public int id() {
-        throw new UnsupportedOperationException("método aún sin implementar");
+       return id;
     }
 
     @Override
@@ -20,52 +61,72 @@ public class Enemigo implements Elemento {
 
     @Override
     public double angulo() {
-        throw new UnsupportedOperationException("método aún sin implementar");
+      return 0;
+      // luego modificar y hacer return angulo;
     }
 
     @Override
     public double x() {
-        throw new UnsupportedOperationException("método aún sin implementar");
+       return x;
     }
 
     @Override
     public double y() {
-        throw new UnsupportedOperationException("método aún sin implementar");
+        return y;
     }
 
     @Override
     public double ancho() {
-        return 0;
+        return ancho;
     }
 
     @Override
     public double alto() {
-        return 0;
+        return alto;
     }
 
     @Override
     public void establecerAncho(double ancho) {
-
+    	this.ancho = ancho;
+        enemigo = enemigoOriginal.getScaledInstance((int) ancho, (int) alto, Image.SCALE_DEFAULT);
     }
 
     @Override
     public void establecerAlto(double alto) {
-
+    	 this.alto = alto;
+         enemigo = enemigoOriginal.getScaledInstance((int) ancho, (int) alto, Image.SCALE_DEFAULT);
     }
 
-    @Override
+  
+    // 0 represennta en angulo que luego tendra que ser cambiado para considerar colision y muerte en caida libre
+    
     public void dibujar(Entorno entorno) {
-        throw new UnsupportedOperationException("método aún sin implementar");
+    	 entorno.dibujarImagen(enemigo, x, y, 0);
     }
 
     @Override
     public void mover(Entorno entorno) {
-
+    	if (angulo == 0 ) {
+    		x = x+ velocidad ;
+    	}
+    	else {
+    		x= x - velocidad ;
+    	}
     }
 
     @Override
     public void actuar(Elemento elemento) {
-        throw new UnsupportedOperationException("método aún sin implementar");
+    	//public void actuar(Elemento elemento) {
+
+    	  //  if (elemento.tipo().equals("proyectil")) {
+    	        // morir
+    	   // }
+
+    	    //if (elemento.tipo().equals("princesa")) {
+    	        // quitar vida
+    	   // }
+    	return ;
+        
     }
 
     @Override
---
+++ src/juego/FabricaEnemigos.java
@@ -0,0 +1,78 @@
+package juego;
+
+import entorno.Entorno;
+
+public class FabricaEnemigos {
+	
+	
+	/*
+	 * Hay que tener en cuenta que no ocupe ninguna posicion de la isla 
+	 * cuando hacemos el random para la posicion hay que chequear que no haya una isla en esa Y
+	 */
+	public int generarPosicion (Isla isla1, Isla isla2, Isla isla3, Isla isla4) {
+		// considero un margen por el alto/grosor de la isla 
+		int margen= 40 ;
+		int y ;
+		y= (int) ( Math.random ()*500);
+		while 
+			( Math.abs( y-isla1.y())<margen || Math.abs(y- isla2.y()) <margen ||
+				Math.abs(y - isla3.y())<margen || Math.abs(y- isla4.y())<margen ) {
+			y= (int) (Math.random ()*500);
+		}
+			return y ;
+	
+	}
+	
+	/*
+	 * Enemigo 1 , lo voy a posicionar del lado izquierdo 
+	 */
+	public Enemigo enemigo1 (GeneradorId generadorId, Entorno entorno, Isla isla1, Isla isla2, Isla isla3, Isla isla4 ) {
+		int x = 25 ;
+		int posicion1 = generarPosicion( isla1, isla2, isla3, isla4);
+		return new Enemigo (generadorId , entorno ,x, posicion1 , 0 );
+	
+	}
+	/* 
+	 * Enemigo 2 se va a crear en el margen del lado derecho por eso usamos como angulo Math.PI= -1
+	 */
+	
+	public Enemigo enemigo2 (GeneradorId generadorId, Entorno entorno ,Isla isla1, Isla isla2, Isla isla3, Isla isla4) {
+		int x= 760;
+		int posicion2 = generarPosicion( isla1, isla2, isla3, isla4);
+		return new Enemigo (generadorId, entorno , x, posicion2, Math.PI) ;
+	}
+	
+	/*
+	 * Enemigo3 se va a crear y posicionar del lado derecho 
+	 * no puede tener la misma posicion con enemigo 1
+	 * con el while lo que hacemos es que se repita la operacion hasta que se produza
+	 * una posicion valida
+	 */
+	public Enemigo enemigo3 (GeneradorId generadorId, Entorno entorno,  
+			Isla isla1, Isla isla2, Isla isla3, Isla isla4, Enemigo enemigo1 ) {
+		int x= 25 ;
+		int posicion3 = generarPosicion (isla1, isla2 ,isla3 , isla4 );
+		
+		while(posicion3 == enemigo1.y()) {
+			posicion3 = generarPosicion(isla1, isla2, isla3, isla4);
+		}
+			
+		return new Enemigo ( generadorId , entorno, x, posicion3, 0 );
+ } 
+	
+	/* 
+	 * Se toma en consideracion no repetir posicion de enemigo2
+	 */
+	public Enemigo enemigo4 (GeneradorId generadorId, Entorno entorno, 
+			Isla isla1, Isla isla2, Isla isla3, Isla isla4, Enemigo enemigo2 ) {
+		int x= 760 ;
+        int posicion3 = generarPosicion (isla1, isla2 ,isla3 , isla4 );
+		
+		while(posicion3 == enemigo2.y()) {
+			posicion3 = generarPosicion(isla1, isla2, isla3, isla4);
+		}
+			
+		return new Enemigo ( generadorId , entorno, x, posicion3, Math.PI );
+	}
+
+}
